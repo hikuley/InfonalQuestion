57fd34af6899038ac67e525e97027aa7ac439f34
hikuley <ssoulless25@gmail.com>
2015-08-23 17:32:19 +0300

    ++

--- src/main/java/com/infonal/controller/HomeController.java
+++ src/main/java/com/infonal/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.infonal.controller;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class HomeController {
 
-    private static final Logger logger = Logger.getLogger(HomeController.class);
+    public static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @RequestMapping(method = RequestMethod.GET)
     public String home(ModelMap model) {
